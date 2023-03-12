882e7e3c4c279a21e434fee19ba635f502dfdebc
Heorhii <gerythepatient@gmail.com>
2023-03-12 18:53:37 +0100

    add two tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const makeGalleryCard = ({ preview,
+  original, description }) => `<a
+        class="gallery__item"
+        href="${original}"
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>`;
+
+const markup = galleryItems.map(el => makeGalleryCard(el)).join('');
+
+const list = document.querySelector('.gallery');
+
+list.insertAdjacentHTML("afterbegin", markup); 
+
+new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
